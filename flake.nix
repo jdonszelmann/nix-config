236cbd4ee9e4c146bf7bc1036692f5c8f7a95703
Rahul Butani <rrbutani@users.noreply.github.com>
2022-09-01 22:56:21 -0500

    flake: first pass at the flake
    this *should* have all the outputs we're interested in listed, at least

---
+++ flake.nix
@@ -0,0 +1,146 @@
+{
+  description = "yo";
+  nixConfig = {
+    bash-prompt = "\[config\]$ ";
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://rrbutani.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "rrbutani.cachix.org-1:FUpcK9RyZjjdOm8qherJl9+wfTGf6ptANvH6LZF63Ro="
+    ];
+  };
+
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    darwin = {
+      url = github:lnl7/nix-darwin/master;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = github:nix-community/home-manager/master;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ragenix = {
+      url = github:yaxitech/ragenix;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flu.url = github:numtide/flake-utils;
+  };
+
+  outputs = inputs@{ self, nixpkgs, darwin, home-manager, ragenix, flu }: let
+    dir = import ./util/list-dir.nix { lib = nixpkgs.lib; };
+    conditionallyProvideInputs = func: inputs:
+      if builtins.isFunction func && (builtins.functionArgs func) != {} then
+        func inputs
+      else func;
+    mapFunc = _: inpPath: conditionallyProvideInputs (import inpPath) inputs;
+    defaultFunc = mapFunc;
+
+    list = [
+      # Outputs tagged with a `<system>`:
+      (flu.lib.eachDefaultSystem (sys: {
+        # TODO: do we have other checks? packages? etc
+        checks = with nixpkgs.lib; let
+          tagAndExtract = { tag, ex }: mapAttrs'
+            (n: v: {
+              name = "${tag}/${n}";
+              value = ex v;
+            })
+            self."${tag}Configurations"
+          ;
+          drvs =
+            (tagAndExtract { tag = "nixos"; ex = d: d.config.system.build.toplevel; }) //
+            (tagAndExtract { tag = "darwin"; ex = d: d.system; }) //
+            (tagAndExtract { tag = "home"; ex = d: d.activationPackage; }); 
+        in
+          filterAttrs (n: v: v.system == sys) drvs;
+
+        # TODO: packages: ./packages
+        # TODO: apps: getExe + ./packages?
+
+        # TODO: devShells?
+      }))
+
+      /* nixpkgs overlays */
+      { overlays = dir { of = ./overlays; inherit mapFunc; }; }
+
+      /* NixOS stuff */
+      { nixosModules = dir {
+        of = ./modules/nixos;
+        recurse = true;
+        mapFunc = _: import;
+      }; }
+      { nixosConfigurations = dir {
+        of = ./machines;
+        includeFilesWithExtension = null;
+        includeDirsWithFile = "nixos.nix";
+
+        # Always make the flake inputs accessible to modules via `specialArgs`:
+        # https://nixos.wiki/wiki/Flakes#Using_nix_flakes_with_NixOS
+        mapFunc = n: v: let
+          config = defaultFunc n v;
+          flakeInputs = inputs;
+          config' = config // { specialArgs = flakeInputs // config.specialArgs or {}; };
+        in
+          nixpkgs.lib.nixosSystem config';
+      }; }
+
+      /* Darwin stuff */
+      { darwinModules = dir {
+        of = ./modules/darwin;
+        recurse = true;
+        mapFunc = _: import;
+      }; }
+      { darwinConfigurations = dir {
+        of = ./machines;
+        includeFilesWithExtension = null;
+        includeDirsWithFile = "darwin.nix";
+
+        # Always make the flake inputs accessible to modules via `inputs`:
+        # https://github.com/LnL7/nix-darwin#flakes-experimental
+        mapFunc = n: v: let
+          config = defaultFunc n v;
+          flakeInputs = inputs;
+          config' = config // { inputs = flakeInputs // config.inputs or {}; };
+        in
+          darwin.lib.darwinSystem config';
+      }; }
+
+      /* home-manager stuff */
+      # TODO: home-manager module key name? See: https://github.com/nix-community/home-manager/issues/1783
+      { homeModules = dir {
+        of = ./modules/home-manager;
+        recurse = true;
+        mapFunc = _: import;
+      }; }
+      { homeConfigurations = dir {
+        of = ./machines;
+        recurse = true;
+        includeFilesWithExtension = "home.nix";
+        includeDirsWithFile = "home.nix";
+
+        # Always make the flake inputs accessible to modules via `extraSpecialArgs`:
+        # https://github.com/nix-community/home-manager/blob/5bd66dc6cd967033489c69d486402b75d338eeb6/templates/standalone/flake.nix#L13-L28
+        mapFunc = n: v: let
+          config = defaultFunc n v;
+          flakeInputs = inputs;
+          pkgs = nixpkgs.legacyPackages.${config.system};
+          config' =
+            (nixpkgs.lib.filterAttrs (n: _: n != "system") config) // { inherit pkgs; } //
+
+            # Prepend to `extraSpecialArgs`:
+            { extraSpecialArgs = flakeInputs // config.extraSpecialArgs or {}; };
+          in
+            home-manager.lib.homeManagerConfiguration config';
+      }; }
+
+      /* lib */
+      { lib = dir { of = ./util; }; }
+
+      # TODO: templates
+    ];
+  in builtins.foldl' (a: b: a // b) { } list;
+}
