671513bb291d9f0915feda3b228e64e525ace984
Rahul Butani <rrbutani@users.noreply.github.com>
2022-09-06 18:36:36 -0500

    flake: add nixos-hardware (a fork, for now)

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
       url = github:yaxitech/ragenix;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-hardware.url = github:rrbutani/nixos-hardware/master; # TODO: switch back to upstream!
     flu.url = github:numtide/flake-utils;
   };
 
