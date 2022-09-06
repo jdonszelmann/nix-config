ee5fc3314f6b6318a024f23c2ad09b13868ce153
Rahul Butani <rrbutani@users.noreply.github.com>
2022-09-06 18:38:18 -0500

    mix: have the `home-manager/users` module read options instead of the `users.users` config
    not as nice an interface as the other way but this avoids cycles (as detailed in 4b6f977c600e4d782aa31e306c9906d70f886827)

--- mixins/home-manager/users/default.nix
+++ mixins/home-manager/users/default.nix
@@ -1,6 +1,5 @@
-{ config, lib, pkgs, home-manager, util, specialArgs, ... }: let
-  # inherit (pkgs.stdenv.hostPlatform) isDarwin;
-  isDarwin = false;
+{ config, lib, pkgs, options, home-manager, util, specialArgs, system, ... }: let
+  inherit (lib.systems.elaborate system) isDarwin;
 in {
   # Add the home-manager module and make the flake inputs accessible to
   # home-manager modules:
@@ -29,45 +28,27 @@ in {
   ];
 
   config = let
+    cfg = config.rrbutani.users;
     dir = util.list-dir { inherit lib; };
 
-    isEnabledUser = if isDarwin then
-      /* for `nix-darwin`, filter on `isHidden` */
-      u: !u.isHidden
-    else
-      /* for `nixos`, filter on `isNormalUser` */
-      u: u.isNormalUser
-    ;
+    hmEnabledUsers = lib.filterAttrs (_: v: v.useHmConfig or false) cfg;
 
     hmUserConfigs = let
       res = dir { of = ./.; mapFunc = _: import; };
     in
       /* leave out `default.nix` (this file) */
       lib.filterAttrs (n: _: n != "default") res;
-    matchedHmUserConfigs = lib.filterAttrs (
-      n: _: (
-        # lib.hasAttr n config.users.users &&
-        # isEnabledUser config.users.users.${n}
-        false
-      )
-    ) hmUserConfigs;
+    matchedHmUserConfigs = lib.mapAttrs (
+      n: _: hmUserConfigs.${n} # we want to throw if we can't find a matching config
+    ) hmEnabledUsers;
   in {
     warnings =
       (lib.optional (matchedHmUserConfigs == {}) ("\n\n" + ''
-        `home-manager/users` was imported but no matching home-manager configs
-        were found for the currently enabled users!
+        `home-manager/users` was imported but no users were specified!
 
         These are the users we have home-manager configs for:
         ${builtins.toString (lib.mapAttrsToList (n: _: "  - `${n}`\n") hmUserConfigs)}
       ''));
-
-#  ++
-#      (lib.optional (enabledUsers == {}) ("\n\n" + ''
-#         No enabled users were detected!
-
-#         Perhaps you included the `home-manager/users` mixin _before_ defining
-#         users in your ${if isDarwin then "nix-darwin" else "nixos"} configuration?
-#       ''));
 
     home-manager.users = matchedHmUserConfigs;
   };
