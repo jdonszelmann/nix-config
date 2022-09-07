0b9b81443538094bf51ea154ed14afff0fd4e96a
Rahul Butani <rrbutani@users.noreply.github.com>
2022-09-07 17:07:09 -0500

    caya: add boot and filesystem config to the top-level config

--- machines/cayahuanca/nixos.nix
+++ machines/cayahuanca/nixos.nix
@@ -8,11 +8,61 @@ inputs@{ nixos-hardware, ragenix, ... }:
 
     # Boot/Encryption:
     {
+      # boot.zfs.enableUnstable = true;
+
+      boot.loader = {
+        efi = {
+          # We want to update the entries in NVRAM!
+          canTouchEfiVariables = true;
+
+          # As per `filesystems.nix`.
+          efiSysMountPoint = "/boot/efi";
+        };
+
+        # We use GRUB for now but we _should_ be able to use systemd-boot,
+        # I think.
+        grub = {
+          enable = true;
+          device = "nodev";
+          version = 2;
+          efiSupport = true;
+          enableCryptodisk = true;
+        };
+      };
+      boot.initrd = {
+        secrets = {
+          # We copied these over manually as part of the installation.
+          #
+          # On a running system, we should be able to get these at `/etc/secrets`
+          # but here we just reference them by their real persistent path so that
+          # we don't need to do anything special for bootstrapping (first
+          # installation).
+          "/etc/secrets/boot.key" = "/persistent/etc/secrets/boot.key";
+          "/etc/secrets/root.key" = "/persistent/etc/secrets/root.key";
+        };
+
+        luks.devices.boot.keyFile = "/etc/secrets/boot.key";
+      };
     }
 
 
     # Filesystems:
     ./filesystems.nix
+    # ZFS stuff:
+    {
+      # TODO: https://nixos.wiki/wiki/ZFS
+      boot.kernelParams = [ "nohibernate" ];
+      services.zfs.autoScrub.enable = true; # TODO
+
+      # `/etc/secrets/root.key` is available in the initrd
+      boot.zfs.requestEncryptionCredentials = true;
+
+      # TODO: snapshots
+
+      networking.hostId = "c9aae02d";
+    }
+      # TODO: zfs snapshot config
+
 
     # Common machine configuration.
     ../../mixins/nixos/laptop.nix
