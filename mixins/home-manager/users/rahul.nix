d6738264ef43a50a98ef3ffd5d0e4b04fb889e05
Rahul Butani <rrbutani@users.noreply.github.com>
2022-09-07 16:23:25 -0500

    mix: add `impermanence` to a `home-manager` user config

--- mixins/home-manager/users/rahul.nix
+++ mixins/home-manager/users/rahul.nix
@@ -1,6 +1,22 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ../../../modules/home-manager/impermanence.nix
+  ];
+
+  rrbutani.impermanence.extra = {
+    dirs = [
+      "downloads"
+      "documents"
+      "dev"
+    ];
+    files = [
+      ".zsh_history"
+      ".bash_history"
+    ];
+  };
+
   home = rec {
     username = "rahul";
     homeDirectory = "/home/${username}";
