5aafcdb3d2ba53d96df581dd1fdda1dcb86f98d8
Rahul Butani <rrbutani@users.noreply.github.com>
2022-09-10 17:44:00 -0500

    mod: spin off some gnome stuff into a gsettings nixos module

--- mixins/nixos/gnome.nix
+++ mixins/nixos/gnome.nix
@@ -1,150 +1,134 @@
 { lib, util, pkgs, ... }: let
   gnome = util.to-gnome-settings { inherit lib; };
+
+  # You can dump these with `dconf dump /`.
+  #
+  # As you're making change in the GUI you can observe them with `dconf watch /`
+  #
+  # See: https://askubuntu.com/questions/522833/how-to-dump-all-dconf-gsettings-so-that-i-can-compare-them-between-two-different
   gsettings = {
-      org.gnome = {
-        desktop = {
-          background = {
-            color-shading-type = "solid";
-            picture-options = "zoom";
-            picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
-            picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
-            primary-color = "#3465a4";
-            secondary-color = "#000000";
-          };
-
-          interface = {
-            color-scheme = "prefer-dark";
-            show-battery-percentage = true;
-          };
-
-          peripherals.touchpad = {
-            speed = 0.027778;
-            tap-to-click = true;
-            two-finger-scrolling-enabled = true;
-          };
-
-          screensaver = {
-            color-shading-type = "solid";
-            lock-delay = gnome.mkUint32 0;
-            picture-options = "zoom";
-            picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
-            primary-color = "#3465a4";
-            secondary-color = "#000000";
-          };
-
-          session.idle-delay = gnome.mkUint32 240;
-
-          sound = {
-            event-sounds = true;
-            theme-name = "__custom";
-          };
-
-          wm.keybindings = {
-            move-to-workspace-left = ["<Alt><Super>Left"];
-            move-to-workspace-right = ["<Alt><Super>Right"];
-            switch-group = ["<Super>grave"];
-            switch-group-backward = ["<Shift><Super>grave"];
-            switch-to-workspace-left = ["<Super>Left"];
-            switch-to-workspace-right = ["<Super>Right"];
-            toggle-fullscreen = ["<Shift><Super>f"];
-          };
+    org.gnome = {
+      desktop = {
+        background = {
+          color-shading-type = "solid";
+          picture-options = "zoom";
+          picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+          picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
+          primary-color = "#3465a4";
+          secondary-color = "#000000";
         };
 
-        mutter = {
-          attach-modal-dialogs = true;
-          dynamic-workspaces = true;
-          edge-tiling = true;
-          focus-change-on-pointer-rest = true;
-          workspaces-only-on-primary = false;
-
-          keybindings = {
-            toggle-tiled-left = ["<Control><Super>Left"];
-            toggle-tiled-right = ["<Control><Super>Right"];
-          };
+        interface = {
+          color-scheme = "prefer-dark";
+          show-battery-percentage = true;
         };
 
-        settings-daemon.plugins = {
-          color = {
-            night-light-enabled = true;
-            night-light-temperature = gnome.mkUint32 2845;
-          };
-
-          media-keys = {
-            custom-keybindings = [
-              "/org/gnome/settings-daemon/plugins/media-keys/user-defined/custom0/"
-            ];
-            next = ["0x100811be"];
-            play = ["Favorites"];
-            previous = ["0x100811bd"];
-            www = ["<Super>b"];
-
-            user-defined = {
-              custom0 = {
-                binding = "<Super>t";
-                command = "kgx";
-                name = "Open Terminal";
-              };
-            };
-          };
-
-          power.power-button-action = "suspend";
+        peripherals.touchpad = {
+          speed = 0.027778;
+          tap-to-click = true;
+          two-finger-scrolling-enabled = true;
         };
 
-        shell = {
-          welcome-dialog-last-shown-version = "42.4";
+        screensaver = {
+          color-shading-type = "solid";
+          lock-delay = gnome.mkUint32 0;
+          picture-options = "zoom";
+          picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+          primary-color = "#3465a4";
+          secondary-color = "#000000";
+        };
 
-          app-switcher.current-workspace-only = true;
+        session.idle-delay = gnome.mkUint32 240;
 
-          keybindings = {
-            show-screen-recording-ui = ["<Shift><Super>r"];
-            show-screenshot-ui = ["<Shift><Super>s"];
-          };
+        sound = {
+          event-sounds = true;
+          theme-name = "__custom";
+        };
 
-          # TODO: move all of these elsewhere (home-manager config? top-level?)
-          favorite-apps = [
-            "firefox.desktop"
-            "org.gnome.Calendar.desktop"
-            "org.gnome.Nautilus.desktop"
-            "org.gnome.Geary.desktop"
-            "spotify.desktop"
-            "teams.desktop"
-            "org.gnome.Photos.desktop"
-            "org.gnome.Console.desktop"
-            "code.desktop"
-            "org.gnome.Settings.desktop"
-            # Move out!!!
-          ];
+        wm.keybindings = {
+          move-to-workspace-left = ["<Alt><Super>Left"];
+          move-to-workspace-right = ["<Alt><Super>Right"];
+          switch-group = ["<Super>grave"];
+          switch-group-backward = ["<Shift><Super>grave"];
+          switch-to-workspace-left = ["<Super>Left"];
+          switch-to-workspace-right = ["<Super>Right"];
+          toggle-fullscreen = ["<Shift><Super>f"];
         };
       };
+
+      mutter = {
+        attach-modal-dialogs = true;
+        dynamic-workspaces = true;
+        edge-tiling = true;
+        focus-change-on-pointer-rest = true;
+        workspaces-only-on-primary = false;
+
+        keybindings = {
+          toggle-tiled-left = ["<Control><Super>Left"];
+          toggle-tiled-right = ["<Control><Super>Right"];
+        };
+      };
+
+      settings-daemon.plugins = {
+        color = {
+          night-light-enabled = true;
+          night-light-temperature = gnome.mkUint32 2845;
+        };
+
+        media-keys = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/user-defined/custom0/"
+          ];
+          next = ["0x100811be"];
+          play = ["Favorites"];
+          previous = ["0x100811bd"];
+          www = ["<Super>b"];
+
+          user-defined = {
+            custom0 = {
+              binding = "<Super>t";
+              command = "kgx";
+              name = "Open Terminal";
+            };
+          };
+        };
+
+        power.power-button-action = "suspend";
+      };
+
+      shell = {
+        welcome-dialog-last-shown-version = "42.4";
+
+        app-switcher.current-workspace-only = true;
+
+        keybindings = {
+          show-screen-recording-ui = ["<Shift><Super>r"];
+          show-screenshot-ui = ["<Shift><Super>s"];
+        };
+
+        # TODO: move all of these elsewhere (home-manager config? top-level?)
+        favorite-apps = [
+          "firefox.desktop"
+          "org.gnome.Calendar.desktop"
+          "org.gnome.Nautilus.desktop"
+          "org.gnome.Geary.desktop"
+          "spotify.desktop"
+          "teams.desktop"
+          "org.gnome.Photos.desktop"
+          "org.gnome.Console.desktop"
+          "code.desktop"
+          "org.gnome.Settings.desktop"
+          # Move out!!!
+        ];
+      };
     };
+  };
 
-    mkCompiledDconf = conf: let
-      str = gnome.toGnomeSettings conf;
-      file = pkgs.writeTextDir "dconf/db" str;
 
-      compile = dir: pkgs.runCommand "dconf-db" { } ''
-        ${pkgs.dconf}/bin/dconf compile $out ${dir}
-      '';
-
-      compiled = compile "${file}/dconf";
-    in
-      "file-db:${compiled}";
 in {
-  # Unfortunately, these settings don't actually take: https://github.com/NixOS/nixpkgs/issues/66554
-  # https://github.com/NixOS/nixpkgs/issues/54150
-  # :-(
-  #
-  # This PR fixes it though, by creating a user dconf config:
-  # https://github.com/NixOS/nixpkgs/pull/189099
-
-  # Until this PR is ready, we just write out the file directly.
-  # https://github.com/NixOS/nixpkgs/pull/189099
-
-  # Because the above PR changes `lib`, swapping it's `dconf` module
-  # into our config (using `disabledModules` to disable our `dconf`
-  # module) is not so straightforward.
-  # disabledModules = [ ];
-  # imports = [ ];
+  imports = [
+    ../../modules/nixos/gsettings.nix
+  ];
 
   # TODO: gate on this being enabled, don't tie to this user..
   #
@@ -166,37 +150,8 @@ in {
   services.xserver.desktopManager.gnome = {
     enable = true;
 
-    # You can dump these with `dconf dump /`.
-    #
-    # As you're making change in the GUI you can observe them with `dconf watch /`
-    #
-    # See: https://askubuntu.com/questions/522833/how-to-dump-all-dconf-gsettings-so-that-i-can-compare-them-between-two-different
     # extraGSettingsOverrides = gsettings;
   };
 
-  /* Stop gap, see above. */
-  # environment.etc."dconf/profile/user-gdm-settings" = {
-  environment.etc."dconf/profile/user" = {
-    text = "user-db:user\n" + (mkCompiledDconf gsettings);
-  };
-  environment.etc."dconf/profile.d/user".text = gnome.toGnomeSettings gsettings;
-  programs.dconf.profiles = lib.mkForce {};
-
-  # This will shadow the default gdm config:
-  # https://github.com/NixOS/nixpkgs/pull/189099/files#diff-f5360a6ed414ff1e292e501143681f1cddea53d4e9ea7ce31ef310e4dc4b378dL232-R237
-  #
-  # So we add it back in as a separate config file:
-  # environment.etc."dconf/profile/gdm-default" = {
-  #   test = gnome.toGnomeSettings {
-  #     org.gnome.settings-daemon.plugins.power = {
-  #       sleep-inactive-ac-type = "nothing";
-  #       sleep-inactive-battery-type = "nothin";
-  #       # sleep-inactive-ac-timeout;
-  #     };
-  #   };
-  # };
-
-  environment.sessionVariables = {
-    DCONF_PROFILE = "user";
-  };
+  rrbutani.gsettings = gsettings;
 }
---
+++ modules/nixos/gsettings.nix
@@ -0,0 +1,86 @@
+# There is a `services.xserver.desktopManager.gnome.extraGSettingsOverrides`
+# option but today this has a couple of "problems":
+#  - it accepts strings, not nix attrsets
+#  - these settings don't actually take:
+#    + https://github.com/NixOS/nixpkgs/issues/66554
+#    + https://github.com/NixOS/nixpkgs/issues/54150
+#
+# This PR fixes both of these issues (the latter by creating a user dconf
+# config): https://github.com/NixOS/nixpkgs/pull/189099
+#
+# This modules is kind of a bad approximation of the fixes in that PR.
+#
+# Because the above PR changes `lib`, swapping it's `dconf` module into our
+# config (using `disabledModules` to disable the regular `dconf` module) is not
+# so straightforward.
+#
+# Instead, this module just writes out the user config file the PR above does,
+# without any of the careful handling around multiple users or defaults.
+#
+# It's not pretty but it's an effective stopgap solution.
+
+{ lib, util, pkgs, config, ... }: let
+  gnome = util.to-gnome-settings { inherit lib; };
+
+  mkCompiledDconf = conf: let
+    str = gnome.toGnomeSettings conf;
+    file = pkgs.writeTextDir "dconf/db" str;
+
+    compile = dir: pkgs.runCommand "dconf-db" { } ''
+      ${pkgs.dconf}/bin/dconf compile $out ${dir}
+    '';
+
+    compiled = compile "${file}/dconf";
+  in
+    "file-db:${compiled}";
+
+  cfg = config.rrbutani.gsettings;
+in {
+  options.rrbutani.gsettings = lib.mkOption {
+    type = lib.types.anything;
+    default = {};
+  };
+
+  config = {
+    assertions = [{
+      assertion = config.services.xserver.desktopManager.gnome.enable;
+      message = ''
+        This module assumes the gnome desktop manager is enabled.
+
+        Enable with:
+          `services.xserver.desktopManager.gnome.enable = true;`
+      '';
+    }];
+
+
+    # TODO: undo these hacks once `https://github.com/NixOS/nixpkgs/pull/189099`
+    # is merged.
+    #
+    # Adding a warning so we remember.
+    warnings = ["Using our custom gsettings module, as a stopgap: https://github.com/NixOS/nixpkgs/pull/189099"];
+
+    # Note that this will shadow the default gdm config:
+    # https://github.com/NixOS/nixpkgs/pull/189099/files#diff-f5360a6ed414ff1e292e501143681f1cddea53d4e9ea7ce31ef310e4dc4b378dL232-R237
+
+    # Normally, `dconf` manages the `/etc/dconf/profiles` directory.
+    #
+    # https://github.com/NixOS/nixpkgs/blob/45b56b5321aed52d4464dc9af94dc1b20d477ac5/nixos/modules/programs/dconf.nix#L51-L53
+    #
+    # We want to suppress this so we forcibly clear dconf's profiles.
+    programs.dconf.profiles = lib.mkForce {};
+
+    environment = {
+      etc = {
+        "dconf/profile/user".text = "user-db:user\n" + (mkCompiledDconf cfg);
+
+        # User readable gschema version too, just so we can see it on the
+        # running system:
+        "dconf/profile.d/user".text = gnome.toGnomeSettings cfg;
+      };
+
+      sessionVariables = {
+        DCONF_PROFILE = "user";
+      };
+    };
+  };
+}
